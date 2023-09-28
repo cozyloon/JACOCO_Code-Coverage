807d9e60e459fab0d0a47a105c8eff622a2da234
Chathumal Sangeeth <38062287+cozyloon@users.noreply.github.com>
2023-09-28 09:46:52 +0530

    Update ResponseUtil.java

--- src/main/java/util/ResponseUtil.java
+++ src/main/java/util/ResponseUtil.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
+// Remove final from the class and static from the method to get 100% code coverage sample
 public final class ResponseUtil {
 
     public static Response getResponse() {
